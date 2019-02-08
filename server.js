8a1ddcdaea510c67cc1a77ae54c3cc7e36a89b27
Muhammad Gad <gadm19@gmail.com>
2019-02-08 11:55:05 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+// const pug = require('pug');
+const fs = require('fs');
+const app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs'); // setting template engine handlebars
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} ${req.method} ${req.url}`;
+  console.log(log);
+  
+    fs.appendFile('server.log', log + '\n', (err) => {
+      if(err) {
+        throw err;
+      }
+      console.log('the data has been appended to the file');
+    });
+  next();
+});
+
+
+// app.use((req, res, next) => {
+  
+//     res.render('maintenance.hbs');
+
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    pageTitle: 'Home Page by Pug engine',
+    paragraph: 'welcome to my Home page'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    pageTitle: 'About Page trasta'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+      errorMessage: 'unable to find page'
+  });
+});
+
+app.listen(3000, () => {
+    console.log("server is up on port 3000.");
+});
